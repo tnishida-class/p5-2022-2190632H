e0ea51134e638189de215f091f4c0720236dca4b
2190632H <2190632h@stu.kobe-u.ac.jp>
2022-11-28 23:38:17 +0900

    Changed Finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,10 +1,32 @@
 // 最終課題を制作しよう
+let img;
+function preload() {
+  img = loadImage('soccerball.jpg');
+}
+let x,y;
+
+function balloon(t,x,y,c)//t=text, c=color
+{
+  let w = textWidth(t);
+  let h = textAscent() + textDescent();
+  let p = 5;
+  fill(c);
+  noStroke();
+  rect(x, y, w + p * 2, h + p * 2);
+  fill(255);
+  textSize(50);
+  text(t, p+x, h + p + y);
+  
+}
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
+  x=600;
+  y=300;
 }
 
 function draw(){
+ 
   background(50);
   fill(100,0,0);
   rect(30, 20, 1120, 450);
@@ -18,16 +40,15 @@ function draw(){
    rect(30, 20, 290,450)
    
   } 
+ image(img, x, y, 200, 200);
+ if(keyIsDown(LEFT_ARROW)){ x -= 5; }
+  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
+  if(keyIsDown(UP_ARROW)){ y -= 5; }
+  if(keyIsDown(DOWN_ARROW)){ y += 5; }
+  if(keyIsDown("A".charCodeAt(0))){ x+= 10; } 
+  if(keyIsDown(" ".charCodeAt(0))){ x-= 10; } 
 
-}
-let img;
-function preload() {
-  img = loadImage('soccerball.jpg');
+  balloon("FIFA World Cup Qatar 2022",250, 200, 10);
 }
 
-function setup() {
-  // Top-left corner of the img is at (0, 0)
-  // Width and height are the img's original width and height
-  
-  image(img, 0, 0);
-}
+
