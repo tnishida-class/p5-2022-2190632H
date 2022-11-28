8d617e76eb574282946bf789aad8d6bfa93c37d6
2190632H <2190632h@stu.kobe-u.ac.jp>
2022-11-28 22:41:15 +0900

    Update final work

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -5,9 +5,29 @@ function setup(){
 }
 
 function draw(){
-  background(160, 192, 255);
+  background(50);
+  fill(100,0,0);
+  rect(30, 20, 1120, 450);
+
+  for(let i = 0; i < 9; i++){
+   //noStroke();
+   noStroke();
+   fill(255);
+   triangle(320, i*50+20 ,320, i*50+70, 400, i*50+45);
+   fill(255);
+   rect(30, 20, 290,450)
+   
+  } 
+
+}
+let img;
+function preload() {
+  img = loadImage('soccerball.jpg');
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+function setup() {
+  // Top-left corner of the img is at (0, 0)
+  // Width and height are the img's original width and height
+  
+  image(img, 0, 0);
 }
